786f15ebbdf55b7010c6c6b036c25507308c272e
spalnique <lex.sydoruk@gmail.com>
2025-02-03 19:43:03 +0100

    added folders structure

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,16 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
+      "@api": path.resolve(__dirname, "./src/api"),
       "@components": path.resolve(__dirname, "./src/components"),
-      "@constant": path.resolve(__dirname, "./src/constant"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
       "@context": path.resolve(__dirname, "./src/context"),
       "@enums": path.resolve(__dirname, "./src/@enums"),
       "@helpers": path.resolve(__dirname, "./src/helpers"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@pages": path.resolve(__dirname, "./src/pages"),
-      "@reduxtoolkit": path.resolve(__dirname, "./src/redux"),
       "@router": path.resolve(__dirname, "./src/router"),
-      "@styled": path.resolve(__dirname, "./src/styled"),
       "@types": path.resolve(__dirname, "./src/@types"),
-      "@websockets": path.resolve(__dirname, "./src/websockets"),
     },
   },
 });
