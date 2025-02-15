0234801a772070d57fa7cef43d35b092324305f2
spalnique <lex.sydoruk@gmail.com>
2025-02-15 16:37:38 +0100

    fix for assets

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,13 +1,11 @@
 import { SVGAttributes } from "react";
 
-import sprite from "../../assets/icons/sprite.svg";
-
 type IconProps = SVGAttributes<SVGSVGElement> & { id: string; size?: number };
 
 const Icon = ({ id, size = 24, ...props }: IconProps) => {
   return (
     <svg width={props.width ?? size} height={props.height ?? size} {...props}>
-      <use href={`${sprite}#${id}`}></use>
+      <use href={`../../assets/icons/sprite.svg#${id}`}></use>
     </svg>
   );
 };
