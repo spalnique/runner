cb7c868f7549482d8ba913ef062a680ca77cad66
spalnique <lex.sydoruk@gmail.com>
2025-02-16 14:21:11 +0100

    removed comments

--- src/hooks/useQuickSearch.ts
+++ src/hooks/useQuickSearch.ts
@@ -22,10 +22,6 @@ const allInitial = {
   // coaches: initial,
 };
 
-// type CompetitionsState = ResponseState<Competition[]>;
-// type AthletesState = ResponseState<Athlete[]>;
-// type CoachesState = ResponseState<Coach[]>;
-
 type Result = {
   competitions: ResponseState<Competition[]>;
   athletes: ResponseState<Athlete[]>;
@@ -34,9 +30,6 @@ type Result = {
 
 export const useQuickSearch = () => {
   const [searchParams] = useSearchParams();
-  // const [competitions, setCompetitions] = useState<CompetitionsState>(initial);
-  // const [athletes, setAthletes] = useState<AthletesState>(initial);
-  // const [coaches, setCoaches] = useState<CoachesState>(initial);
   const [result, setResult] = useState<Result>(allInitial);
 
   useEffect(() => {
@@ -77,65 +70,6 @@ export const useQuickSearch = () => {
         });
     });
   }, [searchParams]);
-
-  // useEffect(() => {
-  //   const text = searchParams.get("text") ?? undefined;
-
-  //   if (!text) return;
-
-  //   setCompetitions((prev) => ({ ...prev, loading: true }));
-  //   setAthletes((prev) => ({ ...prev, loading: true }));
-  //   setCoaches((prev) => ({ ...prev, loading: true }));
-
-  //   getCompetitions({ text, size: 5 })
-  //     .then(({ content, ...responseMeta }) => {
-  //       setCompetitions((prev) => ({
-  //         ...prev,
-  //         content: content.length ? content : null,
-  //         meta: responseMeta,
-  //       }));
-  //     })
-  //     .catch((error) => {
-  //       setCompetitions((prev) => ({ ...prev, error: true }));
-  //       console.error(error);
-  //     })
-  //     .finally(() => {
-  //       setCompetitions((prev) => ({ ...prev, loading: false }));
-  //     });
-
-  //   getAthletes({ nameParts: text, size: 5 })
-  //     .then(({ content, ...responseMeta }) => {
-  //       setAthletes((prev) => ({
-  //         ...prev,
-  //         content: content.length ? content : null,
-  //         meta: responseMeta,
-  //       }));
-  //     })
-  //     .catch((error) => {
-  //       setAthletes((prev) => ({ ...prev, error: true }));
-  //       console.error(error);
-  //     })
-  //     .finally(() => {
-  //       setAthletes((prev) => ({ ...prev, loading: false }));
-  //     });
-
-  //   getCoaches({ text })
-  //     .then((content) => {
-  //       if (content.length > 5) content.length = 5;
-
-  //       setCoaches((prev) => ({
-  //         ...prev,
-  //         content: content.length ? content : null,
-  //       }));
-  //     })
-  //     .catch((error) => {
-  //       setCoaches((prev) => ({ ...prev, error: true }));
-  //       console.error(error);
-  //     })
-  //     .finally(() => {
-  //       setCoaches((prev) => ({ ...prev, loading: false }));
-  //     });
-  // }, [searchParams]);
 
   return result;
 };
