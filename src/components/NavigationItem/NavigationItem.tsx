7179915a1af65ba94dd8638d4c2b86a8918bc4b7
spalnique <lex.sydoruk@gmail.com>
2025-02-16 18:17:19 +0100

    upd navitems querystring issue

--- src/components/NavigationItem/NavigationItem.tsx
+++ src/components/NavigationItem/NavigationItem.tsx
@@ -4,11 +4,13 @@ type NavigationItemProps = { name: string; route: string };
 
 const NavigationItem = ({ name, route }: NavigationItemProps) => {
   const [searchParams] = useSearchParams();
-  const query = "?" + searchParams.toString() || "";
+
+  const query = searchParams.toString();
+  const queryString = query ? `?${query}` : "";
 
   return (
     <NavLink
-      to={route + query}
+      to={route + queryString}
       className={({ isActive }) =>
         (isActive && "border-b-2 border-b-blue-700 font-normal text-gray-700") +
         " flex h-full items-center justify-center px-5 capitalize transition-all hover:bg-blue-600 hover:text-white"
