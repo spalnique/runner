eee2e6a7c4d155ea3278e63780aab2d719bba1b7
spalnique <lex.sydoruk@gmail.com>
2025-02-18 22:39:01 +0100

    fix

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as HomePage } from "./HomePage/HomePage.tsx";
-export { default as CompetitionsPage } from "./CompetitionsPage/CompetitionsPage.tsx";
-export { default as AthletesPage } from "./AthletesPage/AthletesPage.tsx";
-export { default as CoachesPage } from "./CoachesPage/CoachesPage.tsx";
+export { default as HomePage } from './HomePage/HomePage.tsx';
+export { default as CompetitionsPage } from './CompetitionsPage/CompetitionsPage.tsx';
+export { default as AthletesPage } from './AthletesPage/AthletesPage.tsx';
+export { default as CoachesPage } from './CoachesPage/CoachesPage.tsx';
