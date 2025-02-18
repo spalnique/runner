13ca003aa03b91a03b8beac4997297c1eda99ebc
spalnique <lex.sydoruk@gmail.com>
2025-02-18 23:16:45 +0100

    add coaches search page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,10 @@
-import { ComponentPropsWithRef } from "react";
+import { ComponentPropsWithRef } from 'react';
 
-type Props = ComponentPropsWithRef<"main">;
+type Props = ComponentPropsWithRef<'main'>;
 
 const Main = ({ children }: Props) => {
   return (
-    <div className="flex-grow bg-gray-800 bg-[url('./assets/images/backgrounds/bg-running-track-stadium-1920.webp')] bg-cover bg-center bg-no-repeat bg-blend-screen">
+    <div className="flex-grow bg-gray-800 bg-[url('./assets/images/backgrounds/bg-running-track-forest-1920.webp')] bg-cover bg-center bg-no-repeat bg-blend-screen">
       {children}
     </div>
   );
--- src/pages/CoachesPage/CoachesPage.tsx
+++ src/pages/CoachesPage/CoachesPage.tsx
@@ -1,6 +1,50 @@
-import { Main } from "@components";
+import { ChangeEventHandler } from 'react';
+
+import {
+  CoachesSearchResults,
+  Main,
+  SearchInput,
+  SearchResultTitle,
+  SearchResultWrapper,
+  Section,
+} from '@components';
+import { useQueryContext } from '@contexts';
+import { useDebounceCall } from '@hooks';
+import { QueryParams } from '@types';
 
 const CoachesPage = () => {
-  return <Main>Coaches page</Main>;
+  const { textQuery, setTextQuery } = useQueryContext();
+
+  const params: QueryParams = { text: textQuery, size: 20 };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setTextQuery(target.value);
+  };
+
+  const debouncedHandleChange = useDebounceCall(handleChange);
+
+  return (
+    <Main>
+      <Section>
+        <SearchInput
+          key={textQuery}
+          defaultValue={textQuery ?? ''}
+          placeholder="Search by coach"
+          onChange={debouncedHandleChange}
+          autoFocus
+        />
+
+        {textQuery && (
+          <SearchResultWrapper>
+            <SearchResultTitle
+              title="Coach search results"
+              className="bg-blue-700 font-medium text-white"
+            />
+            <CoachesSearchResults params={params} paginated={params.size > 5} />
+          </SearchResultWrapper>
+        )}
+      </Section>
+    </Main>
+  );
 };
 export default CoachesPage;
